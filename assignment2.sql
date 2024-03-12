267dd87528d8c0b782c9e3065239dee16c4637e3
Yaroslava Mala <ymala@kse.org.ua>
2024-03-12 14:53:40 +0200

    added index for appointment_clone table

--- assignment2.sql
+++ assignment2.sql
@@ -207,3 +207,14 @@ SELECT e.id AS employee_id, e.name AS employee_name, s.name AS service_name, a.d
     WHERE a.date BETWEEN '2024-02-10' AND '2024-02-17'
     AND s.name = 'Coloring'
     ORDER BY a.date;
+
+-- index (bonus task)
+
+CREATE TABLE appointment_clone LIKE appointment;
+CREATE INDEX index_employee_id ON appointment_clone(employee_id);
+
+INSERT INTO appointment_clone (customer_id, service_id, employee_id, date, status)
+SELECT customer_id, service_id, employee_id, date, status FROM appointment;
+
+SELECT * FROM appointment WHERE employee_id = 1;
+SELECT * FROM appointment_clone WHERE employee_id = 1;
