d7dff9f81eea6193fc6913ce2fd4a79dd585f5db
Yaroslava Mala <ymala@kse.org.ua>
2024-03-22 10:35:14 +0200

    added 4 more selects and changed * in selects

--- assignment2.sql
+++ assignment2.sql
@@ -238,61 +238,128 @@ FROM
 -- HW3
 -- with non-correlated subqueries result
 
-SELECT * FROM services WHERE duration = (SELECT MIN(duration) FROM services);
-UPDATE services SET price = 3000 WHERE duration = (SELECT MAX(duration) FROM (SELECT duration FROM services) AS subquery);
-DELETE FROM product WHERE quantity = (SELECT MIN(quantity) FROM (SELECT quantity FROM product) AS subquery);
+SELECT name, description, duration, price
+FROM services
+WHERE duration = (SELECT MIN(duration) FROM services);
 
-SELECT * FROM employee WHERE id IN (SELECT employee_id FROM employee_service WHERE service_id = 1);
-UPDATE product SET quantity = quantity + 10 WHERE id IN (SELECT product_id FROM services2products WHERE service_id = 2);
-DELETE FROM payment WHERE appointment_id IN (
-    SELECT id FROM appointment WHERE customer_id IN (SELECT id FROM customer WHERE phone = '+3809501843819'));
-DELETE FROM appointment WHERE customer_id IN (SELECT id FROM customer WHERE phone = '+3809501843819');
+UPDATE services SET price = 3000
+WHERE duration = (SELECT MAX(duration) FROM (SELECT duration FROM services) AS subquery);
 
-SELECT * FROM employee WHERE id NOT IN (SELECT employee_id FROM appointment);
-UPDATE employee SET salary = salary * 1.1 WHERE id NOT IN (SELECT employee_id FROM employee_service WHERE service_id > 3);
-DELETE FROM product WHERE id NOT IN (SELECT product_id FROM services2products);
+]DELETE FROM product
+WHERE quantity = (SELECT MIN(quantity) FROM (SELECT quantity FROM product) AS subquery);
 
-SELECT * FROM services WHERE EXISTS (SELECT 1 FROM services2products WHERE product_id = 3);
-UPDATE services SET price = price * 1.1 WHERE EXISTS (SELECT * FROM product WHERE price > 2000);
-DELETE FROM services2products WHERE EXISTS (SELECT 1 FROM product WHERE quantity = 0);
+SELECT name, phone, position, salary
+FROM employee
+WHERE id IN (SELECT employee_id FROM employee_service WHERE service_id = 1);
 
-SELECT * FROM product WHERE NOT EXISTS (SELECT * FROM services2products WHERE product_id > 7);
-UPDATE employee SET salary = salary * 1.1 WHERE NOT EXISTS (
-    SELECT 1 FROM (SELECT * FROM employee) AS subquery WHERE subquery.phone = '+380630768334');
-DELETE FROM product WHERE NOT EXISTS (SELECT 1 FROM services2products WHERE product_id < 6);
+UPDATE product SET quantity = quantity + 10
+WHERE id IN (SELECT product_id FROM services2products WHERE service_id = 2);
+
+DELETE FROM payment
+WHERE appointment_id IN (
+    SELECT id FROM appointment
+    WHERE customer_id IN (SELECT id FROM customer WHERE phone = '+3809501843819'));
+
+DELETE FROM appointment
+WHERE customer_id IN (SELECT id FROM customer WHERE phone = '+3809501843819');
+
+SELECT name, phone, email, salary
+FROM employee
+WHERE id NOT IN (SELECT employee_id FROM appointment);
+
+UPDATE employee SET salary = salary * 1.1
+WHERE id NOT IN (SELECT employee_id FROM employee_service WHERE service_id > 3);
+
+DELETE FROM product
+WHERE id NOT IN (SELECT product_id FROM services2products);
+
+SELECT name, description, price
+FROM services
+WHERE EXISTS (SELECT 1 FROM services2products WHERE product_id = 3);
+
+UPDATE services SET price = price * 1.1
+WHERE EXISTS (SELECT * FROM product WHERE price > 2000);
+
+DELETE FROM services2products
+WHERE EXISTS (SELECT 1 FROM product WHERE quantity = 0);
+
+
+SELECT name, description, quantity
+FROM product
+WHERE NOT EXISTS (SELECT * FROM services2products WHERE product_id > 7);
+
+UPDATE employee SET salary = salary * 1.1
+WHERE NOT EXISTS (SELECT 1 FROM (SELECT * FROM employee) AS subquery WHERE subquery.phone = '+380630768334');
+
+DELETE FROM product
+WHERE NOT EXISTS (SELECT 1 FROM services2products WHERE product_id < 6);
 
 -- with correlated subqueries result
 
-SELECT c.* FROM customer c WHERE c.id = (
+SELECT c.name, c.phone, c.eamil
+FROM customer c
+WHERE c.id = (
     SELECT a.customer_id FROM appointment a
-                                  JOIN services s ON a.service_id = s.id
+                                  INNER JOIN services s ON a.service_id = s.id
     WHERE s.name = 'Haircut'
     ORDER BY a.date LIMIT 1
     );
-UPDATE employee e SET e.salary = e.salary * 1.1 WHERE e.id = (
+
+UPDATE employee e
+SET e.salary = e.salary * 1.1
+WHERE e.id = (
     SELECT a.employee_id FROM appointment a
     GROUP BY a.employee_id ORDER BY COUNT(a.id) DESC
     LIMIT 1
     );
 
-SELECT e.* FROM employee e WHERE e.id IN (SELECT employee_id FROM appointment WHERE date >= '2024-02-14' AND employee_id = e.id);
-UPDATE employee e SET e.salary = e.salary * 1.1 WHERE e.id IN (
+SELECT e.name, e.phone, e.salary
+FROM employee e
+WHERE e.id IN (SELECT employee_id FROM appointment WHERE date >= '2024-02-14' AND employee_id = e.id);
+
+UPDATE employee e
+SET e.salary = e.salary * 1.1
+WHERE e.id IN (
     SELECT es.employee_id FROM employee_service es
-                                   JOIN services s ON es.service_id = s.id WHERE s.price > 2000
-                                                                             AND es.employee_id = e.id
+                                   JOIN services s ON es.service_id = s.id
+    WHERE s.price > 2000
+      AND es.employee_id = e.id
 );
-DELETE FROM services2products sp WHERE sp.product_id IN (SELECT p.id FROM product p WHERE p.id = sp.product_id AND p.name = 'Botox');
 
-SELECT c.* FROM customer c WHERE c.id NOT IN (SELECT a.customer_id FROM appointment a WHERE a.customer_id = c.id);
-UPDATE product p SET p.quantity = 0 WHERE p.id NOT IN
-                                          (SELECT sp.product_id FROM services2products sp WHERE sp.product_id = p.id);
-DELETE FROM appointment a WHERE a.id NOT IN (SELECT p.appointment_id FROM payment p WHERE p.appointment_id = a.id);
+DELETE FROM services2products sp
+WHERE sp.product_id IN (SELECT p.id FROM product p WHERE p.id = sp.product_id AND p.name = 'Botox');
 
-SELECT s.* FROM services s WHERE EXISTS (SELECT 1 FROM appointment a WHERE a.service_id = s.id);
-UPDATE services s SET s.price = s.price * 1.1 WHERE EXISTS (SELECT 1 FROM appointment a WHERE a.service_id = s.id);
-DELETE FROM services2products sp WHERE EXISTS (SELECT 1 FROM appointment a WHERE a.service_id = sp.service_id);
+SELECT c.name, c.phone, c.email
+FROM customer c
+WHERE c.id NOT IN (SELECT a.customer_id FROM appointment a WHERE a.customer_id = c.id);
 
-SELECT p.* FROM product p WHERE NOT EXISTS (SELECT 1 FROM services2products sp WHERE sp.product_id = p.id);
-UPDATE employee e SET e.salary = e.salary * 0.8 WHERE NOT EXISTS
-                                                          (SELECT 1 FROM employee_service es WHERE es.employee_id = e.id);
-DELETE FROM product p WHERE NOT EXISTS (SELECT * FROM services2products sp WHERE sp.product_id = p.id);
+UPDATE product p
+SET p.quantity = 0
+WHERE p.id NOT IN
+      (SELECT sp.product_id FROM services2products sp WHERE sp.product_id = p.id);
+
+DELETE FROM appointment a
+WHERE a.id NOT IN (SELECT p.appointment_id FROM payment p WHERE p.appointment_id = a.id);
+
+SELECT s.name, s.description, s.price
+FROM services s
+WHERE EXISTS (SELECT 1 FROM appointment a WHERE a.service_id = s.id);
+
+UPDATE services s
+SET s.price = s.price * 1.1
+WHERE EXISTS (SELECT 1 FROM appointment a WHERE a.service_id = s.id);
+
+DELETE FROM services2products sp
+WHERE EXISTS (SELECT 1 FROM appointment a WHERE a.service_id = sp.service_id);
+
+SELECT p.name, p.description, p.quantity
+FROM product p
+WHERE NOT EXISTS (SELECT 1 FROM services2products sp WHERE sp.product_id = p.id);
+
+UPDATE employee e
+SET e.salary = e.salary * 0.8
+WHERE NOT EXISTS
+          (SELECT 1 FROM employee_service es WHERE es.employee_id = e.id);
+
+DELETE FROM product p
+WHERE NOT EXISTS (SELECT * FROM services2products sp WHERE sp.product_id = p.id);
--- pa3_bonus/main.py
+++ pa3_bonus/main.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 db_connection = mysql.connector.connect(
   host="localhost",
-  user="yarrochka",
-  passwd="password12345",
+  user="root", # or yarrochka
+  passwd="test_pass",  # or password12345
   database="beauty_salon"
 )
 
@@ -18,7 +18,12 @@ print(f"Inserted service ID: {cursor.lastrowid}")
 select_query = "SELECT * FROM services WHERE price > %s"
 cursor.execute(select_query, (1000,))
 for service in cursor.fetchall():
-    print(service)
+    print(f"ID: {service[0]}")
+    print(f"Name: {service[1]}")
+    print(f"Description: {service[2]}")
+    print(f"Duration: {service[3]} minutes")
+    print(f"Price: ${service[4]:.2f}")
+    print("-" * 20)
 
 update_query = "UPDATE services SET price = %s WHERE name = %s"
 cursor.execute(update_query, (1600.00, "Deep Cleansing"))
