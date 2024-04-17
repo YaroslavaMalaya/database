54df49a500c1f5763df3e8ec7e85dc259f212ab3
Yaroslava Mala <ymala@kse.org.ua>
2024-04-17 17:14:53 +0300

    changed logic for finding employee schedule

--- pa5/views.sql
+++ pa5/views.sql
@@ -11,8 +11,11 @@ CREATE INDEX index_a_date ON appointment(date);
 CREATE VIEW employee_schedule AS
 SELECT
     e.name AS employee_name,
-    GROUP_CONCAT(DISTINCT s.name ORDER BY s.id SEPARATOR ', ') AS services_provided,
-    CONCAT( DATE_FORMAT(MIN(a.date), '%H:%i'), '-', DATE_FORMAT(MAX(DATE_ADD(a.date, INTERVAL s.duration MINUTE)), '%H:%i')) AS time_from_to,
+    (SELECT GROUP_CONCAT(DISTINCT s2.name ORDER BY s2.id SEPARATOR ', ')
+     FROM employee_service es2
+        JOIN services s2 ON es2.service_id = s2.id
+            WHERE es2.employee_id = e.id) AS services_provided,
+    CONCAT(DATE_FORMAT(a.date, '%H:%i'), '-', DATE_FORMAT(DATE_ADD(a.date, INTERVAL s.duration MINUTE), '%H:%i')) AS time_from_to,
     CASE
         WHEN 'Haircut' IN (SELECT s2.name FROM employee_service es2 JOIN services s2 ON es2.service_id = s2.id WHERE es2.employee_id = e.id) OR
              'Coloring' IN (SELECT s2.name FROM employee_service es2 JOIN services s2 ON es2.service_id = s2.id WHERE es2.employee_id = e.id)
@@ -24,12 +27,11 @@ SELECT
         THEN 'Cosmetologist'
         ELSE 'General staff'
         END AS kind_of_service
-    FROM employee e 
-        INNER JOIN employee_service es ON e.id = es.employee_id
-        INNER JOIN services s ON es.service_id = s.id
+    FROM employee e
         INNER JOIN appointment a ON e.id = a.employee_id
+        INNER JOIN services s ON a.service_id = s.id
     WHERE
-        DATE(a.date) = CURDATE()
-        GROUP BY e.id ORDER BY FIELD(e.position, 'Master', 'Trainee') DESC;
+        DATE(a.date) = '2024-02-14'
+        ORDER BY FIELD(e.position, 'Master', 'Trainee') DESC;
 
 SELECT * FROM employee_schedule;
